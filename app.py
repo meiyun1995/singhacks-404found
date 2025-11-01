bb475bd3b793af30cfc2bc112f7c4c20c5ae3d8c
AddChew <addison_chew@outlook.com>
2025-11-01 10:48:39 +0800

    update rest mehtod

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
 from etl import Transaction
+from pydantic import BaseModel
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from fastapi import FastAPI, HTTPException
+
+
+class Request(BaseModel):
+    transaction_id: str
 
 
 app = FastAPI()
@@ -10,9 +15,11 @@ engine = create_engine(f"sqlite:///data/singhacks.db")
 Session = sessionmaker(bind=engine)
 
 
-@app.get("/transactions/{transaction_id}")
-def get_transaction(transaction_id):
+@app.post("/transactions/analyze")
+def get_transaction(request: Request):
     # TODO: add agent pipeline here
     with Session() as session:
-        transaction = session.query(Transaction).filter_by(transaction_id=transaction_id).first()
-        return transaction.to_dict() if transaction else None
+        transaction = session.query(Transaction).filter_by(transaction_id = request.transaction_id).first()
+        if not transaction:
+            raise HTTPException(status_code = 404, detail = f"Transaction {request.transaction_id} does not exist.")
+        return transaction.to_dict()
