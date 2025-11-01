d4ab42fe77dd8372a135b98cf2d0ce9b115e7605
AddChew <addison_chew@outlook.com>
2025-11-01 11:00:31 +0800

    add dotenv

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
+import os
+
 from etl import Transaction
 from pydantic import BaseModel
+from dotenv import load_dotenv
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from fastapi import FastAPI, HTTPException
+
+
+load_dotenv()
+
+if not os.getenv("OPENAI_API_KEY"):
+    raise ValueError(
+        "OPENAI_API_KEY not found in environment variables. Please check your .env file."
+    )
+
+if not os.getenv("OPENAI_BASE_URL"):
+    raise ValueError(
+        "OPENAI_API_KEY not found in environment variables. Please check your .env file."
+    )
 
 
 class Request(BaseModel):
@@ -18,6 +34,7 @@ Session = sessionmaker(bind=engine)
 @app.post("/transactions/analyze")
 def get_transaction(request: Request):
     # TODO: add agent pipeline here
+    # TODO: specify model on agent level instead of project level
     with Session() as session:
         transaction = session.query(Transaction).filter_by(transaction_id = request.transaction_id).first()
         if not transaction:
