c300ef2d4d2dfbce8bc3bf03631d5ce6479b4fce
AddChew <addison_chew@outlook.com>
2025-11-01 22:14:10 +0800

    add part 1 app

--- ui/app.py
+++ ui/app.py
@@ -11,11 +11,16 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 db = pd.read_csv("../data/transactions_mock_1000_for_participants.csv").fillna("").replace({"": None})
-live_transactions = db[db.transaction_id == "ad66338d-b17f-47fc-a966-1b4395351b41"].to_dict(orient= "records")
-live_transactions[0]["ai_risk_score"] = 0.9
-live_transactions[0]["transaction_time"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+live_transactions = []
+initial_txn = db[db.transaction_id == "ad66338d-b17f-47fc-a966-1b4395351b41"].to_dict(orient= "records")[0]
+initial_txn["ai_risk_score"] = 0.65
+initial_txn["transaction_time"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+live_transactions.append(initial_txn)
 pending_transactions = db[~(db.transaction_id == "ad66338d-b17f-47fc-a966-1b4395351b41")].to_dict(orient="records")
 
+with open("../reports/RPT-20251101-0001_ad66338d-b17f-47fc-a966-1b4395351b41.text", "r") as f:
+    cached_report = f.read()
 
 @app.get("/")
 async def dashboard(request: Request):
@@ -24,29 +29,16 @@ async def dashboard(request: Request):
         {
             "request": request,
             "total_transactions": len(live_transactions),
-            "action_required": sum(1 for t in live_transactions if t["ai_risk_score"] > 0.75),
+            "action_required": sum(1 for t in live_transactions if t["ai_risk_score"] >= 0.65),
             "transactions": live_transactions,
         },
     )
 
 @app.get("/transaction/{transaction_id}", name="transaction_detail")
 async def transaction_detail(request: Request, transaction_id: str):
-    # TODO: replace with DB
-    transaction = {
-        "transaction_id": transaction_id,
-        "transaction_amount": 15000.50,
-        "transaction_currency": "USD",
-        "transaction_channel": "Online",
-        "transaction_product_type": "FX",
-        "beneficiary_country": "SG",
-        "customer_type": "Corporate",
-        "customer_risk_rating": "High",
-        "client_risk_profile": "Aggressive",
-        "ai_risk_score": 0.82,
-    }
     return templates.TemplateResponse(
         "transaction_detail.html",
-        {"request": request, "transaction": transaction},
+        {"request": request, "transaction": initial_txn, "transaction_report_text": cached_report},
     )
 
 @app.get("/stream/transactions")
